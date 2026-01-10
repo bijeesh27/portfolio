96939652f74f74d96eb97ce4579be9ba34aa8465
bijeesh27 <bijeeshbiju3669@gmail.com>
2026-01-10 16:48:47 +0530

    change favicon

--- src/hooks/useTheme.js
+++ src/hooks/useTheme.js
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react';
 
 const useTheme = () => {
-  const [theme, setTheme] = useState('light');
+  const [theme, setTheme] = useState('dark');
 
   const toggleTheme = () => {
     if (theme === 'light') {
